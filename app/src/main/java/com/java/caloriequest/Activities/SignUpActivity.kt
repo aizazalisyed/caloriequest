c7672b601685d09fdf770d6f20aadd961223b3f8
aizazalisyed <aizazalisyed.outlook.com>
2023-08-26 14:48:45 +0500

    first commit

---
+++ app/src/main/java/com/java/caloriequest/Activities/SignUpActivity.kt
@@ -0,0 +1,12 @@
+package com.java.caloriequest.Activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.java.caloriequest.R
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
